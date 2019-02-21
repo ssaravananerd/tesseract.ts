652115dea340ab8a01774111aaa30516e76f2ebc
ssaravananerd <ssaravananerd@gmail.com>
2019-02-21 12:49:07 +0530

    Got error in node_module - Cannot find module 'tesseract.js'.
    I got an error while importing the tesseract module in our project module. 
    ERROR in node_modules/tesseract.ts/dist/index.d.ts(1,33): error TS2307: Cannot find module 'tesseract.js'.  
    
    I searched in google, some one has suggested to modify the index.d.ts file in node_module in tesseract.ts folder. After doing that change it is working charm. But this is not a right way to do a modification on files in nodule_module. So I would help to fix this issue in this package. Kindly let me know if this is the fix or I'm referring a wrong package.
    
    Thanks

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
-import * as TesseractTypes from "tesseract.js";
+//import * as TesseractTypes from "tesseract.js";
+import * as TesseractTypes from 'tesseract.js/dist/tesseract'; 
 export declare const Tesseract: TesseractTypes.TesseractStatic;
